0a369c1a982cc1bbe3f0b04ab93b88659f4af57e
AzzaFazza <A.Fallon@kainos.com>
2014-07-20 03:49:37 +0100

    added No task

---
+++ Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-JazzHands.build/Objects-normal/i386/IFTTTScaleAnimation.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/adamf/Desktop/trackly/Pods/JazzHands/src/IFTTTScaleAnimation.m \
+  /Users/adamf/Desktop/trackly/Pods/JazzHands/src/IFTTTScaleAnimation.h \
+  /Users/adamf/Desktop/trackly/Pods/JazzHands/src/IFTTTAnimation.h \
+  /Users/adamf/Desktop/trackly/Pods/JazzHands/src/IFTTTAnimationKeyFrame.h \
+  /Users/adamf/Desktop/trackly/Pods/JazzHands/src/IFTTTAnimationFrame.h
---
+++ Build/Intermediates/Taskly.build/Debug-iphoneos/Taskly.build/Objects-normal/armv7s/customSeque.d
@@ -0,0 +1,2 @@
+dependencies: /Users/adamf/Desktop/trackly/trackly/customSeque.m \
+  /Users/adamf/Desktop/trackly/trackly/customSeque.h
